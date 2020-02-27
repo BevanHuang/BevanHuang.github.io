83c3c48db2171ff34e06751a501402b4011e788c
BevanHuang <BevanHuang@users.noreply.github.com>
2020-02-27 18:07:12 +0800

    Add files via upload

--- Practice/day2/src/service/movieService.js
+++ Practice/day2/src/service/movieService.js
@@ -1,17 +1,21 @@
-// 用于获取电影的数据
 export default {
-    // 获取多条数据
     async getMovies(page, limit) {
-        var url = `http://yuanjin.tech:5005/api/movie?page=${page}&limit=${limit}`;
-        var datas = await fetch(url).then(resp => resp.json());
-        return datas;
+      return new Promise(resolve => {
+        setTimeout(async () => {
+          var url = `http://yuanjin.tech:5005/api/movie?page=${page}&limit=${limit}`;
+          var datas = await fetch(url).then(resp => resp.json());
+          resolve(datas);
+        }, 1000);
+      });
     },
-    // 获取一条数据
     async getMovie(id) {
-        var url = `http://yuanjin.tech:5005/api/movie/${id}`;
-        var datas = await fetch(url).then(resp =>
-        resp.json()
-        );
-        return datas;
+      return new Promise(resolve => {
+        setTimeout(async () => {
+          var url = `http://yuanjin.tech:5005/api/movie/${id}`;
+          var datas = await fetch(url).then(resp => resp.json());
+          resolve(datas);
+        }, 1000);
+      });
     }
-};
+  };
+  
